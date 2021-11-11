8e8adb8d168f3cb3192f4213eb8e95cbbbf29faf
Victor Natschke <2229521+xheuz@users.noreply.github.com>
2021-11-11 16:18:04 -0400

    Feature/define contracts (#2)
    * Updated README documentation
    
    * Added draft for trustee and trust contract
    
    * Added test files for contracts
    
    * Added migration for trustee contract

---
+++ contracts/Trust.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract Trust is Ownable, Pausable {
+    address payable private beneficiary;
+
+    constructor(address payable _beneficiary) {
+      beneficiary = _beneficiary;
+    }
+}
