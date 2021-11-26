95f7e6cf73e799b4bd9fb07fbd563e678b6d413a
Victor Natschke <vnatschke@gmail.com>
2021-11-26 17:28:18 -0400

    Updated truffle configuration and added .env.example file

--- truffle-config.js
+++ truffle-config.js
@@ -1,19 +1,23 @@
+require("dotenv").config();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
 const path = require("path");
 
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-  compilers: {
-    solc: {
-      version: "0.8.10"
-    }
-  },
+  compilers: { solc: { version: "0.8.10" } },
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      host: '127.0.0.1',
+      host: "127.0.0.1",
       port: 8545,
-      network_id: '*'
-    }
-  }
+      network_id: "*",
+    },
+    ropsten: {
+      provider: () =>
+        new HDWalletProvider({
+          mnemonic: process.env.MNEMONIC,
+          providerOrUrl: process.env.INFURA_ROPSTEN,
+        }),
+      network_id: 3,
+    },
+  },
 };
